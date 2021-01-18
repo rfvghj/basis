5121631d7d80b62466e958b410406cd3a9499120
marui <1016595756@qq.com>
2021-01-18 16:00:48 +0800

    first commit

---
+++ precache-manifest.e2fb6b7b1a4ac683c8dc0d3aa832a3d3.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef61ac6c3beaf21537fc4898a5aca94",
+    "url": "./index.html"
+  },
+  {
+    "revision": "522eda3793995628f72d",
+    "url": "./static/css/main.cf602f5d.chunk.css"
+  },
+  {
+    "revision": "7cfb786db4d07b8a1558",
+    "url": "./static/js/0.82556c27.chunk.js"
+  },
+  {
+    "revision": "2b8a863b0bfce9858cde",
+    "url": "./static/js/10.1a05e3a2.chunk.js"
+  },
+  {
+    "revision": "68e518c988d74b991e17",
+    "url": "./static/js/3.df9ceee6.chunk.js"
+  },
+  {
+    "revision": "8453d9fe2d085f91b84da6086e8a4acb",
+    "url": "./static/js/3.df9ceee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a366b7948a9ae16b59",
+    "url": "./static/js/4.f1412e9d.chunk.js"
+  },
+  {
+    "revision": "232075edf90b960c5e43",
+    "url": "./static/js/5.3875bb76.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "./static/js/5.3875bb76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b109be025eaaa8d94a8",
+    "url": "./static/js/6.2569fa92.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "./static/js/6.2569fa92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385cfddbba52bc53d942",
+    "url": "./static/js/7.09672a83.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "./static/js/7.09672a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fafd4cc864fb3a70abb9",
+    "url": "./static/js/8.88815e36.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "./static/js/8.88815e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2536ffecbadc3625c84",
+    "url": "./static/js/9.9019e330.chunk.js"
+  },
+  {
+    "revision": "522eda3793995628f72d",
+    "url": "./static/js/main.b04f3ab5.chunk.js"
+  },
+  {
+    "revision": "1244d113d45613735ff7",
+    "url": "./static/js/runtime-main.fd7161fc.js"
+  },
+  {
+    "revision": "17db7fc28ea8f9cc1b19f24c67b10c60",
+    "url": "./static/media/DAI.17db7fc2.png"
+  },
+  {
+    "revision": "c76b33ca42c5730ab77f3341ce9764a7",
+    "url": "./static/media/USDC.c76b33ca.png"
+  },
+  {
+    "revision": "839a0f0d6e0a69fed8d7846344d2100b",
+    "url": "./static/media/USDT.839a0f0d.png"
+  },
+  {
+    "revision": "80fb5592c2e3b926434d5356087e51b0",
+    "url": "./static/media/basis-bond-logo.80fb5592.svg"
+  },
+  {
+    "revision": "89e2967875c805010eda6e2a11917411",
+    "url": "./static/media/basis-cash-logo.89e29678.svg"
+  },
+  {
+    "revision": "466cf09b6dd8d18a5c0b2d083d2bfe78",
+    "url": "./static/media/basis-share-logo.466cf09b.svg"
+  },
+  {
+    "revision": "0a327c746280fd93f2b4ba3efe83c878",
+    "url": "./static/media/farmer.0a327c74.png"
+  },
+  {
+    "revision": "f8fec857dc3b8e244e4c910c550b1df8",
+    "url": "./static/media/sUSD.f8fec857.png"
+  },
+  {
+    "revision": "88ae1ab3f40535ebbd248d03a7aab473",
+    "url": "./static/media/ycrv.88ae1ab3.png"
+  }
+]);
